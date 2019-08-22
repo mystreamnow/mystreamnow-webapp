5eb426525f3e22043c0638edb615c64c350af550
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2019-08-21 23:26:58 -0300

    Enviar convite

--- src/components/Player/BarControllers/Invite/Invite.js
+++ src/components/Player/BarControllers/Invite/Invite.js
@@ -1,18 +1,109 @@
 import React, { Fragment } from 'react';
-import { Tooltip, Grid, Icon } from '@material-ui/core';
-import IconButton from '@material-ui/core/IconButton';
+
+import {
+  DialogTitle,
+  DialogContentText,
+  DialogContent,
+  Tooltip,
+  Grid,
+  Icon,
+  Button,
+  TextField,
+  Dialog,
+  DialogActions,
+  IconButton,
+  useMediaQuery,
+  Slide,
+  Fab
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Send from '@material-ui/icons/Send';
+
+import { useTheme } from '@material-ui/core/styles';
+
+const Transition = React.forwardRef(function Transition (props, ref) {
+  return <Slide direction='up' ref={ref} {...props} />;
+});
+
+const useStyles = makeStyles(theme => ({
+  Send: {
+    marginRight: theme.spacing(1)
+  }
+}));
 
 const Invite = () => {
+  const [open, setOpen] = React.useState(false);
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const classes = useStyles();
+
+  function handleSendInvite () {
+    setOpen(true);
+  }
+
+  function handleClose () {
+    setOpen(false);
+  }
+
+  function handleInvite () {
+    setOpen(true);
+    console.log('handleInvite');
+  }
+
   return (
     <Fragment>
       <Tooltip title='Convidar participante'>
         <Grid>
-          <IconButton disableRipple className='btn-icons-btn'>
+          <IconButton
+            onClick={handleInvite}
+            disableRipple
+            className='btn-icons-btn'
+          >
             <Icon className='btn-icons'>launch</Icon>
             <span className='btn-label'>Convidar</span>
           </IconButton>
         </Grid>
       </Tooltip>
+
+      <Dialog
+        fullScreen={fullScreen}
+        fullWidth
+        maxWidth='md'
+        TransitionComponent={Transition}
+        keepMounted
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='form-dialog-title'
+      >
+        <DialogTitle id='form-dialog-title'>Enviar convite</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Digite o e-mail da pessoa que você deseja convidar:
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin='dense'
+            id='email'
+            label='Endereço de email'
+            type='email'
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color='secondary'>
+            Cancelar
+          </Button>
+          <Fab
+            aria-label='send'
+            variant='extended'
+            onClick={handleSendInvite}
+            color='primary'
+          >
+            <Send className={classes.Send} />
+            Enviar
+          </Fab>
+        </DialogActions>
+      </Dialog>
     </Fragment>
   );
 };
